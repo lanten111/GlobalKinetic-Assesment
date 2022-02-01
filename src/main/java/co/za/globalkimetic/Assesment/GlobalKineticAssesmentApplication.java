0a205adba940dad9f11497de1931bd2d96b3ced9
Makhado <muhumbulo.makhado@momentum.co.za>
2022-02-01 14:00:15 +0200

    initial commit

---
+++ src/main/java/co/za/globalkimetic/Assesment/GlobalKineticAssesmentApplication.java
@@ -0,0 +1,13 @@
+package co.za.globalkimetic.Assesment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobalKineticAssesmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GlobalKineticAssesmentApplication.class, args);
+    }
+
+}
