d8cb383ef519818569cc3ebacce5be24c2a3806f
Makhado <muhumbulo.makhado@momentum.co.za>
2022-02-04 22:17:43 +0200

    added user repo

---
+++ src/main/java/co/za/globalkimetic/Assesment/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package co.za.globalkimetic.Assesment.repository;
+
+import co.za.globalkimetic.Assesment.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<Long, User> {
+
+}
